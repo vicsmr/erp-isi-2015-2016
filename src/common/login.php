a587eb29808ab1a5b7b98cdb2ae9b5bb53e83dc8
J. M. Colmenar <jmcolmenar@gmail.com>
2015-11-05 15:59:33 +0100

    Comunicación jQuery completa en los dos sentidos.
    Se incluye la inserción de HTML en el contenedor web de la portada.

--- src/common/login.php
+++ src/common/login.php
@@ -22,7 +22,24 @@
  *  @source code https://github.com/jmcolmenar/erp-isi-2015-2016
  */
 
+include_once("functions.inc");
+
+
+/*
+ * 1.- Comprobar que usuario y pass están en la BD.
+ * 1.1.- Activar la sesióny guardar en ella info de seguridad.
+ * 2.- Recabar el listado de módulos a los que se tiene acceso.
+ * 3.- Codificarlos y enviarlos.
+ */
+
+// Los datos vienen en POST
+//$_POST["user"]
+//$_POST["pass"]
+
+// Dummy: siempre los mismos módulos.
+
 $json_result = array();
-$json_result["Result"] = "Servidor respondiendo OK";
+$json_result["0"] = main_menu_element("Mod. Test 1","modules/testmod1/");
+$json_result["1"] = main_menu_element("Mod. Test 2","modules/testmod2/");
 
 print json_encode($json_result);
